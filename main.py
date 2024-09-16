8e66b3f07d296c50071a5a06351c13c07e025243
takuya kinoshita <kktak02@gmail.com>
2024-09-16 10:49:47 +0900

    add path parameters

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "hello fast api."}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: int):
+    return {"user_id": user_id}
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep learning FTW!"}
+    
+    if model_name is ModelName.resnet:
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
