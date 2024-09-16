386031a929850e271684fe865e186fc80657c9c7
takuya kinoshita <kktak02@gmail.com>
2024-09-16 11:15:19 +0900

    列挙型メンバの追加

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ async def get_model(model_name: ModelName):
     if model_name is ModelName.alexnet:
         return {"model_name": model_name, "message": "Deep learning FTW!"}
     
-    if model_name is ModelName.resnet:
+    if model_name.value == "lenet":
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
