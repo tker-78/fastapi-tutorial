400f82e78a60c87b13c3817c8247ed2f727fa7f3
takuya kinoshita <kktak02@gmail.com>
2024-09-16 15:23:37 +0900

    クエリパラメータの完了

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from fastapi import FastAPI
 from enum import Enum
+from typing import Union
 
 app = FastAPI()
+
+# 最初のステップ
 
 @app.get("/")
 async def root():
     return {"message": "hello fast api."}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+# パスパラメータ
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: int):
+#     return {"item_id": item_id}
 
 @app.get("/users/me")
 async def read_user_me():
@@ -34,3 +39,40 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+
+
+# クエリパラメータ
+
+fake_items_db = [
+    {"item_name": "Foo"},
+    {"item_name": "Bar"},
+    {"item_name": "Baz"}
+]
+
+@app.get("/items")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip:skip+limit] # http://localhost:8000/items?skip=1&limit=2
+
+@app.get("/item/{item_id}")
+async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description."}
+        )
+    return item
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long descrtiption."}
+        )
+    return item
