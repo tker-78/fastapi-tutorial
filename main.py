c62cbc84aa41fe5d7b6ac178ea2a6abf079f87e5
takuya kinoshita <kktak02@gmail.com>
2024-09-21 15:31:01 +0900

    add new chapter

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query, Path
+from fastapi import FastAPI, Query, Path, Body
 from enum import Enum
 from typing import Union, List
 
@@ -131,3 +131,28 @@ async def read_items(
         results.update({"q": q})
     return results
 
+# ボディ-複数のパラメータ
+# @app.put("/items/{item_id}")
+# async def update_item(
+#     *,
+#     item_id: int = Path(title="The ID of the item to get", ge=0, le=100),
+#     q: Union[str, None] = None,
+#     item: Union[Item, None] = None,
+# ):
+#     results = {"item_id": item_id}
+#     if q:
+#         results.update({"q": q})
+#     if item:
+#         results.update({"item": item})
+#     return results
+
+
+class User(BaseModel):
+    username: str
+    full_name: Union[str, None] = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item = Body(embed=True)):
+    results = {"item_id": item_id, "item": item }
+    return results
